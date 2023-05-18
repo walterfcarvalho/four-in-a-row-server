ef78c4ee18598e21054b5262f00d49eef34c10b4
Valter f Carvalho <walterfcarvalho@gmail.com>
2023-05-18 17:22:33 +0100

    config to build

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import env from 'dotenv'
 
 env.config()
 
-const port: string | undefined = process.env.PORT || "3002"
+const port: string  = process.env.PORT || "3002"
 
 app.listen( parseInt(port), '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${process.env.PORT}`);
